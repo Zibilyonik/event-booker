a07dee96ce3c1b6439cb47daed594b0c0d2aea40
Onur <onatsurmeli@gmail.com>
2025-02-06 19:51:55 +0100

    import CalendarComponent into AppComponent, clean the template file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'event-booker';
+
 }
